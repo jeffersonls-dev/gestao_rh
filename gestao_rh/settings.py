1715533aac6146582eb91a428091189fa9877d7d
Jefferson Silva <jefferson-ls1@hotmail.com>
2019-09-19 11:56:53 -0300

    x

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'wmy(n$=uo7%d%b23_ah)q+ywi-f6d!y%2d4pvp5ctf6c8su)c$'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.228.137.236']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
